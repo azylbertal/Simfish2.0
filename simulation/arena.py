fixed random number generation in simulation, logging rewards and events, same reward scaling for positive and negative energy change

simulation/arena.py
@@ -20,9 +20,9 @@ class Arena:
     """Class used to create a 2D image of the environment and surrounding features, and use this to compute photoreceptor
     inputs"""
 
-    def __init__(self, env_variables):
+    def __init__(self, env_variables, rng):
         self.test_mode = env_variables['test_sensory_system']
-
+        self.rng = rng
         self.bottom_intensity = env_variables['bottom_intensity']
         self.max_uv_range = np.round(np.absolute(np.log(0.001) / env_variables["light_decay_rate"])).astype(np.int32)
 
@@ -60,7 +60,7 @@ class Arena:
             new_grating[self.arena_height // 2 + 40: self.arena_height // 2 + 120, self.arena_width // 2 + 20: self.arena_width // 2 + 100] = 10.
 
         else:
-            new_grating = np.random.rand(self.arena_width, self.arena_height)
+            new_grating = self.rng.random((self.arena_width, self.arena_height))
             new_grating = gaussian_filter(new_grating, sigma=self.sediment_sigma)
             new_grating -= np.min(new_grating)
             new_grating /= np.max(new_grating)

simulation/eye.py
@@ -17,10 +17,10 @@ import geometry
 
 class Eye:
 
-    def __init__(self, verg_angle, retinal_field, is_left, env_variables, max_uv_range,):
+    def __init__(self, verg_angle, retinal_field, is_left, env_variables, max_uv_range,rng):
 
         self.test_mode = env_variables["test_sensory_system"]
-        
+        self.rng = rng
         self.viewing_elevations = env_variables["viewing_elevations"]
         self.fish_elevation = env_variables["elevation"]
         self.uv_object_intensity = env_variables["uv_object_intensity"]
@@ -231,7 +231,7 @@ class Eye:
     def add_noise_to_readings(self, readings):
         """Samples from Poisson distribution to get number of photons"""
         if self.env_variables["shot_noise"]:
-            photons = np.random.poisson(readings)
+            photons = self.rng.poisson(readings)
         else:
             photons = readings
 

simulation/fish.py
@@ -17,61 +17,18 @@ import pymunk
 
 from simulation.eye import Eye
 
-def draw_angle_dist(bout_id):
-    if bout_id == 0:  # Slow2
-        mean = [2.49320953e+00, 2.36217665e-19]
-        cov = [[4.24434912e-01, 1.89175382e-18],
-                [1.89175382e-18, 4.22367139e-03]]
-    elif bout_id == 1 or bout_id == 2:  # RT
-        mean = [2.74619216, 0.82713249]
-        cov = [[0.3839484,  0.02302918],
-               [0.02302918, 0.03937928]]
-    elif bout_id == 3:  # sCS
-        mean = [0.956603146, -6.86735892e-18]
-        cov = [[2.27928786e-02, 1.52739195e-19],
-               [1.52739195e-19, 3.09720798e-03]]
-    elif bout_id == 4 or bout_id == 5:  # J-turn 1
-        mean = [0.49074911, 0.39750791]
-        cov = [[0.00679925, 0.00071446],
-               [0.00071446, 0.00626601]]
-    elif bout_id == 6:  # do nothing
-        mean = [0, 0]
-        cov = [[0, 0],
-               [0, 0]]
-    elif bout_id == 7 or bout_id == 8:  # C-Start
-        mean = [7.03322223, 0.67517832]
-        cov = [[1.35791922, 0.10690938],
-               [0.10690938, 0.10053853]]
-    elif bout_id == 9:  # AS
-        mean = [6.42048088e-01, 1.66490488e-17]
-        cov = [[3.99909515e-02, 3.58321400e-19],
-               [3.58321400e-19, 3.24366068e-03]]
-
-    elif bout_id == 10 or bout_id == 11:  # J-turn 2
-        mean = [1.0535197,  0.61945679]
-        # cov = [[ 0.0404599,  -0.00318193],
-        #        [-0.00318193,  0.01365224]]
-        cov = [[0.0404599,  0.0],
-               [0.0,  0.01365224]]
-    else:
-        mean = [0, 0],
-        cov = [[0, 0],
-               [0, 0]]
-        print("Draw action error")
-
-    bout_vals = np.random.multivariate_normal(mean, cov, 1)
-    return bout_vals[0, 1], bout_vals[0, 0], mean[1], mean[0]
 
 class Fish:
 
 
-    def __init__(self, env_variables, max_uv_range):
+    def __init__(self, env_variables, max_uv_range, rng):
 
         # For the purpose of producing a calibration curve.
         inertia = pymunk.moment_for_circle(env_variables['fish_mass'], 0, env_variables['fish_head_radius'], (0, 0))
         self.max_uv_range = max_uv_range
         self.env_variables = env_variables
         self.body = pymunk.Body(1, inertia)
+        self.rng = rng
 
         # Mouth
         self.mouth = pymunk.Circle(self.body, env_variables['fish_mouth_radius'], offset=(0, 0))
@@ -100,9 +57,9 @@ class Fish:
 
 
         self.left_eye = Eye(self.verg_angle, self.retinal_field, True, env_variables,
-                            max_uv_range=self.max_uv_range)
+                            max_uv_range=self.max_uv_range, rng=self.rng)
         self.right_eye = Eye(self.verg_angle, self.retinal_field, False, env_variables,
-                             max_uv_range=self.max_uv_range)
+                             max_uv_range=self.max_uv_range, rng=self.rng)
 
         self.hungry = 0
         self.stress = 1
@@ -121,8 +78,7 @@ class Fish:
         self.a_scaling_energy_cost = self.env_variables['a_scaling_energy_cost']
         self.baseline_energy_use = self.env_variables['baseline_energy_use']
 
-        self.action_reward_scaling = self.env_variables['action_reward_scaling']
-        self.consumption_reward_scaling = self.env_variables['consumption_reward_scaling']
+        self.action_energy_reward_scaling = self.env_variables['action_energy_reward_scaling']
 
         if "action_energy_use_scaling" in self.env_variables:
             self.action_energy_use_scaling = self.env_variables["action_energy_use_scaling"]
@@ -138,12 +94,54 @@ class Fish:
 
         self.deterministic_action = self.env_variables['deterministic_action']
 
+    def draw_angle_dist(self, bout_id):
+        if bout_id == 0:  # Slow2
+            mean = [2.49320953e+00, 2.36217665e-19]
+            cov = [[4.24434912e-01, 1.89175382e-18],
+                    [1.89175382e-18, 4.22367139e-03]]
+        elif bout_id == 1 or bout_id == 2:  # RT
+            mean = [2.74619216, 0.82713249]
+            cov = [[0.3839484,  0.02302918],
+                [0.02302918, 0.03937928]]
+        elif bout_id == 3:  # sCS
+            mean = [0.956603146, -6.86735892e-18]
+            cov = [[2.27928786e-02, 1.52739195e-19],
+                [1.52739195e-19, 3.09720798e-03]]
+        elif bout_id == 4 or bout_id == 5:  # J-turn 1
+            mean = [0.49074911, 0.39750791]
+            cov = [[0.00679925, 0.00071446],
+                [0.00071446, 0.00626601]]
+        elif bout_id == 6:  # do nothing
+            mean = [0, 0]
+            cov = [[0, 0],
+                [0, 0]]
+        elif bout_id == 7 or bout_id == 8:  # C-Start
+            mean = [7.03322223, 0.67517832]
+            cov = [[1.35791922, 0.10690938],
+                [0.10690938, 0.10053853]]
+        elif bout_id == 9:  # AS
+            mean = [6.42048088e-01, 1.66490488e-17]
+            cov = [[3.99909515e-02, 3.58321400e-19],
+                [3.58321400e-19, 3.24366068e-03]]
+
+        elif bout_id == 10 or bout_id == 11:  # J-turn 2
+            mean = [1.0535197,  0.61945679]
+            # cov = [[ 0.0404599,  -0.00318193],
+            #        [-0.00318193,  0.01365224]]
+            cov = [[0.0404599,  0.0],
+                [0.0,  0.01365224]]
+        else:
+            raise ValueError(f"Unknown bout_id: {bout_id}")
+
+        bout_vals = self.rng.multivariate_normal(mean, cov, 1)
+        return bout_vals[0, 1], bout_vals[0, 0], mean[1], mean[0]
+
     def take_action(self, action):
         reward = 0
         if self.env_variables['test_sensory_system']:
             self.body.angle += 0.1
         if not (action == 6 or self.env_variables['test_sensory_system']):
-            angle_change, distance, mean_angle, mean_distance = draw_angle_dist(action)
+            angle_change, distance, mean_angle, mean_distance = self.draw_angle_dist(action)
             if self.deterministic_action:
                 angle_change = mean_angle
                 distance = mean_distance
@@ -152,7 +150,6 @@ class Fish:
                 angle_change = -angle_change
 
             if action == 3:
-                #reward -= self.env_variables['capture_swim_extra_cost']
                 self.making_capture = True
 
             self.prev_action_angle = angle_change
@@ -189,7 +186,7 @@ class Fish:
 
     def update_energy_level(self, reward, consumption):
         """Updates the current energy state for continuous and discrete fish."""
-        energy_intake = 1.0 * consumption
+        energy_change = consumption
 
         if self.action_energy_use_scaling == "Nonlinear":
             energy_use = self.i_scaling_energy_cost * (abs(self.prev_action_impulse) ** 2) + \
@@ -204,17 +201,15 @@ class Fish:
                          self.a_scaling_energy_cost * (abs(self.prev_action_angle) ** 0.5) + \
                          self.baseline_energy_use
         else:
-            energy_use = self.i_scaling_energy_cost * (abs(self.prev_action_impulse) ** 0.5) + \
-                         self.a_scaling_energy_cost * (abs(self.prev_action_angle) ** 0.5) + \
-                         self.baseline_energy_use
-
-
+            raise ValueError(f"Unknown action energy use scaling: {self.action_energy_use_scaling}")
+            
         if self.prev_action == 3:
             energy_use *= self.env_variables['capture_swim_energy_cost_scaling']
-        reward += (energy_intake * self.consumption_reward_scaling) - (energy_use * self.action_reward_scaling)
+        
+        energy_change -= energy_use
 
-        self.energy_level += energy_intake - energy_use
-        if self.energy_level > 1.0:
-            self.energy_level = 1.0
+        reward += energy_change * self.action_energy_reward_scaling
+        self.energy_level += energy_change
 
+        self.energy_level = min(self.energy_level, 1.0)  # Ensure energy level does not exceed 1.0
         return reward